16622d519bfb60966949dd4507292b16dd910c25
haisu-2 <115346281+haisu-2@users.noreply.github.com>
2022-10-22 12:31:05 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,13 +1,13 @@
 function checkTemp(temp) {
-if (temp < 20 && temp > 0) {
+if (temp <= 20 && temp >= 0) {
         console.log("-1")
         return -1
     }
-     else if ( temp < 40 && temp > 21 ) {
+     else if ( temp <= 40 && temp >= 21 ) {
         console.log("0")
-        return 0
+         return 0
     }
-    else if (temp < 60 && temp > 41) {
+    else if (temp <= 60 && temp >= 41) {
         console.log("1") 
         return 1
     }
